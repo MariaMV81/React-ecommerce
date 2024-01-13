42397d47558b8c7988802e6f563c4f646ff0ca75
MariaMV81 <mariazuaga31@gmail.com>
2024-01-13 11:16:25 +0100

    Probando MaterialUI

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,3 +1,11 @@
+import {Typography} from '@mui/material'
+import Container from "@mui/material/Container";
+import Button from "@mui/material/Button";
+import AirplanemodeActiveIcon from "@mui/icons-material/AirplanemodeActive";
+import AndroidIcon from "@mui/icons-material/Android";
+import Grid from "@mui/material/Grid";
+
+
 export default function Footer(){
     return(
         <>
@@ -35,6 +43,107 @@ export default function Footer(){
             </div>
 
         </footer>
+
+            <Typography
+                variant="h1"
+                component="h2"
+            >
+                Soy un h1
+            </Typography>
+            <Typography variant="h2">Soy un h2</Typography>
+            <Typography variant="h3">Soy un h3</Typography>
+            <Typography variant="h4">Soy un h4</Typography>
+
+            <Container>
+                <Button
+                    variant="contained"
+                    color="info"
+                    startIcon={<AirplanemodeActiveIcon />}
+                >
+                    Botón personalizado
+                </Button>
+
+                <Button
+                    variant="contained"
+                    color="secondary"
+                    endIcon={<AndroidIcon />}
+                >
+                    Botón 2
+                </Button>
+            </Container>
+
+            <Container>
+                <Grid
+                    container
+                    spacing={8}
+                >
+                    <Grid
+                        item
+                        xs={12}
+                        sm={6}
+                        md={4}
+                    >
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+                            aspernatur perferendis ipsum veniam, nostrum eos quaerat sequi sed,
+                            quisquam minima provident, ad magnam saepe impedit voluptatem
+                            ratione quas molestias! Nisi.
+                        </p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={6}
+                        md={4}
+                    >
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+                            aspernatur perferendis ipsum veniam, nostrum eos quaerat sequi sed,
+                            quisquam minima provident, ad magnam saepe impedit voluptatem
+                            ratione quas molestias! Nisi.
+                        </p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={4}
+                    >
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+                            aspernatur perferendis ipsum veniam, nostrum eos quaerat sequi sed,
+                            quisquam minima provident, ad magnam saepe impedit voluptatem
+                            ratione quas molestias! Nisi.
+                        </p>
+                    </Grid>
+                    <Grid
+                        item
+                        xs={12}
+                        sm={12}
+                        md={4}
+                    >
+                        <p>
+                            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Qui
+                            aspernatur perferendis ipsum veniam, nostrum eos quaerat sequi sed,
+                            quisquam minima provident, ad magnam saepe impedit voluptatem
+                            ratione quas molestias! Nisi.
+                        </p>
+                    </Grid>
+                </Grid>
+            </Container>
+            <Typography
+                variant="h1"
+                component="h2"
+                mb={2}
+                color="primary"
+                align="center"
+                boxShadow={3}
+                pb={2}
+            >
+                Soy un h1
+            </Typography>
+
+
         </>
     )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,32 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import 'bootstrap/dist/css/bootstrap.min.css'
+import { CssBaseline } from "@mui/material";//aseguran de que todos los componentes tengan una apariencia consistente.
+
+import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+const theme = createTheme({
+  palette: {
+    primary: {
+      main: "#000e35",
+    },
+    secondary: {
+      main: "#f50057",
+    },
+  },
+});
+
+import "@fontsource/roboto/300.css";
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/500.css";
+import "@fontsource/roboto/700.css";
+
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <CssBaseline />
+    <ThemeProvider theme={theme}>
     <App />
+    </ThemeProvider>
   </React.StrictMode>,
 )
